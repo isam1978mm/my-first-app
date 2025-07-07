d1e850a16dd38b052581e501edef846dfa75ecdf
Isam Matragi <your-email@example.com>
2025-07-06 23:05:28 -0400

    Fix: valid React component in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+'use client';
+
 export default function Home() {
   return (
     <main>
-      <h1>Hello from my first Next.js project!</h1>
+      <h1>Hello from my first Next.js app!</h1>
     </main>
   );
 }
